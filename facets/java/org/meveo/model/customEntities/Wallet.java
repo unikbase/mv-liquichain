def72476de78f5abe1d37d480a4466c7490b0247
meveo.admin <no-mail@meveo.com>
2021-12-17 04:09:13 +0000

    Add the cet java source file : Wallet.javain the module : LIQUICHAIN

---
+++ facets/java/org/meveo/model/customEntities/Wallet.java
@@ -0,0 +1,127 @@
+package org.meveo.model.customEntities;
+
+import org.meveo.model.CustomEntity;
+import java.util.List;
+import org.meveo.model.persistence.DBStorageType;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.meveo.model.customEntities.LiquichainApp;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+public class Wallet implements CustomEntity {
+
+    public Wallet() {
+    }
+
+    public Wallet(String uuid) {
+        this.uuid = uuid;
+    }
+
+    private String uuid;
+
+    @JsonIgnore()
+    private DBStorageType storages;
+
+    private String accountHash;
+
+    @JsonProperty(required = true)
+    private LiquichainApp application;
+
+    private String balance;
+
+    private String hexHash;
+
+    @JsonProperty(required = true)
+    private String name;
+
+    private String publicInfo;
+
+    private String applicationInstanceUUID;
+
+    private String publicKey;
+
+    @Override()
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public DBStorageType getStorages() {
+        return storages;
+    }
+
+    public void setStorages(DBStorageType storages) {
+        this.storages = storages;
+    }
+
+    public String getAccountHash() {
+        return accountHash;
+    }
+
+    public void setAccountHash(String accountHash) {
+        this.accountHash = accountHash;
+    }
+
+    public LiquichainApp getApplication() {
+        return application;
+    }
+
+    public void setApplication(LiquichainApp application) {
+        this.application = application;
+    }
+
+    public String getBalance() {
+        return balance;
+    }
+
+    public void setBalance(String balance) {
+        this.balance = balance;
+    }
+
+    public String getHexHash() {
+        return hexHash;
+    }
+
+    public void setHexHash(String hexHash) {
+        this.hexHash = hexHash;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPublicInfo() {
+        return publicInfo;
+    }
+
+    public void setPublicInfo(String publicInfo) {
+        this.publicInfo = publicInfo;
+    }
+
+    public String getApplicationInstanceUUID() {
+        return applicationInstanceUUID;
+    }
+
+    public void setApplicationInstanceUUID(String applicationInstanceUUID) {
+        this.applicationInstanceUUID = applicationInstanceUUID;
+    }
+
+    public String getPublicKey() {
+        return publicKey;
+    }
+
+    public void setPublicKey(String publicKey) {
+        this.publicKey = publicKey;
+    }
+
+    @Override()
+    public String getCetCode() {
+        return "Wallet";
+    }
+}
