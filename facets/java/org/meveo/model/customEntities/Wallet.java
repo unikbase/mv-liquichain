a26444fc2f0a118395ab9e2f7c231123127d3e11
meveo.admin <no-mail@meveo.com>
2022-01-26 01:24:59 +0000

    Add the cet java source file : Wallet.javain the module : LIQUICHAIN

--- facets/java/org/meveo/model/customEntities/Wallet.java
+++ facets/java/org/meveo/model/customEntities/Wallet.java
@@ -39,6 +39,8 @@ public class Wallet implements CustomEntity {
 
     private String applicationInstanceUUID;
 
+    private String keyPair;
+
     private String publicKey;
 
     private List<GroupPurchase> groupPurchases = new ArrayList<>();
@@ -114,6 +116,14 @@ public class Wallet implements CustomEntity {
 
     public void setApplicationInstanceUUID(String applicationInstanceUUID) {
         this.applicationInstanceUUID = applicationInstanceUUID;
+    }
+
+    public String getKeyPair() {
+        return keyPair;
+    }
+
+    public void setKeyPair(String keyPair) {
+        this.keyPair = keyPair;
     }
 
     public String getPublicKey() {
