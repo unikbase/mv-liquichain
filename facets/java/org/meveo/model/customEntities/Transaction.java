f98eb6038b272d9f654b217969c337e06579e592
meveo.admin <no-mail@meveo.com>
2022-02-07 23:18:40 +0000

    Add the cet java source file : Transaction.javain the module : LIQUICHAIN

--- facets/java/org/meveo/model/customEntities/Transaction.java
+++ facets/java/org/meveo/model/customEntities/Transaction.java
@@ -33,6 +33,8 @@ public class Transaction implements CustomEntity {
 
     private Instant creationDate;
 
+    private String type;
+
     private String nonce;
 
     private String gasLimit;
@@ -120,6 +122,14 @@ public class Transaction implements CustomEntity {
 
     public void setCreationDate(Instant creationDate) {
         this.creationDate = creationDate;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
     }
 
     public String getNonce() {
