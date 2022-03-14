86ae26a4d3e5b6658978e619e3157a0d0b848b07
meveo.admin <no-mail@meveo.com>
2022-03-14 07:58:07 +0000

    Add the cet java source file : LiquichainApp.javain the module : mv-liquichain

--- facets/java/org/meveo/model/customEntities/LiquichainApp.java
+++ facets/java/org/meveo/model/customEntities/LiquichainApp.java
@@ -3,6 +3,8 @@ package org.meveo.model.customEntities;
 import org.meveo.model.CustomEntity;
 import java.util.List;
 import org.meveo.model.persistence.DBStorageType;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.time.Instant;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 public class LiquichainApp implements CustomEntity {
@@ -19,6 +21,28 @@ public class LiquichainApp implements CustomEntity {
     @JsonIgnore()
     private DBStorageType storages;
 
+    private String upgradeRules;
+
+    private String hexCode;
+
+    private String previousHash;
+
+    @JsonProperty(required = true)
+    private String name;
+
+    private String description;
+
+    private String registrationRules;
+
+    private String iconUrl;
+
+    private Instant creationDate;
+
+    private String version;
+
+    @JsonProperty(required = true)
+    private String shortCode;
+
     @Override()
     public String getUuid() {
         return uuid;
@@ -34,6 +58,86 @@ public class LiquichainApp implements CustomEntity {
 
     public void setStorages(DBStorageType storages) {
         this.storages = storages;
+    }
+
+    public String getUpgradeRules() {
+        return upgradeRules;
+    }
+
+    public void setUpgradeRules(String upgradeRules) {
+        this.upgradeRules = upgradeRules;
+    }
+
+    public String getHexCode() {
+        return hexCode;
+    }
+
+    public void setHexCode(String hexCode) {
+        this.hexCode = hexCode;
+    }
+
+    public String getPreviousHash() {
+        return previousHash;
+    }
+
+    public void setPreviousHash(String previousHash) {
+        this.previousHash = previousHash;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getRegistrationRules() {
+        return registrationRules;
+    }
+
+    public void setRegistrationRules(String registrationRules) {
+        this.registrationRules = registrationRules;
+    }
+
+    public String getIconUrl() {
+        return iconUrl;
+    }
+
+    public void setIconUrl(String iconUrl) {
+        this.iconUrl = iconUrl;
+    }
+
+    public Instant getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Instant creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public String getShortCode() {
+        return shortCode;
+    }
+
+    public void setShortCode(String shortCode) {
+        this.shortCode = shortCode;
     }
 
     @Override()
