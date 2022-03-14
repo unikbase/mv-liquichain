f0b757527f744f844a22290932a4df70e59d2809
meveo.admin <no-mail@meveo.com>
2022-03-14 07:56:23 +0000

    Add the cet java source file : Wallet.javain the module : mv-liquichain

--- facets/java/org/meveo/model/customEntities/Wallet.java
+++ facets/java/org/meveo/model/customEntities/Wallet.java
@@ -3,10 +3,6 @@ package org.meveo.model.customEntities;
 import org.meveo.model.CustomEntity;
 import java.util.List;
 import org.meveo.model.persistence.DBStorageType;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import org.meveo.model.customEntities.LiquichainApp;
-import org.meveo.model.customEntities.GroupPurchase;
-import java.util.ArrayList;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 public class Wallet implements CustomEntity {
@@ -23,26 +19,6 @@ public class Wallet implements CustomEntity {
     @JsonIgnore()
     private DBStorageType storages;
 
-    private String accountHash;
-
-    private String privateKey;
-
-    @JsonProperty(required = true)
-    private LiquichainApp application;
-
-    private String balance;
-
-    @JsonProperty(required = true)
-    private String name;
-
-    private String publicInfo;
-
-    private String applicationInstanceUUID;
-
-    private String publicKey;
-
-    private List<GroupPurchase> groupPurchases = new ArrayList<>();
-
     @Override()
     public String getUuid() {
         return uuid;
@@ -58,78 +34,6 @@ public class Wallet implements CustomEntity {
 
     public void setStorages(DBStorageType storages) {
         this.storages = storages;
-    }
-
-    public String getAccountHash() {
-        return accountHash;
-    }
-
-    public void setAccountHash(String accountHash) {
-        this.accountHash = accountHash;
-    }
-
-    public String getPrivateKey() {
-        return privateKey;
-    }
-
-    public void setPrivateKey(String privateKey) {
-        this.privateKey = privateKey;
-    }
-
-    public LiquichainApp getApplication() {
-        return application;
-    }
-
-    public void setApplication(LiquichainApp application) {
-        this.application = application;
-    }
-
-    public String getBalance() {
-        return balance;
-    }
-
-    public void setBalance(String balance) {
-        this.balance = balance;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPublicInfo() {
-        return publicInfo;
-    }
-
-    public void setPublicInfo(String publicInfo) {
-        this.publicInfo = publicInfo;
-    }
-
-    public String getApplicationInstanceUUID() {
-        return applicationInstanceUUID;
-    }
-
-    public void setApplicationInstanceUUID(String applicationInstanceUUID) {
-        this.applicationInstanceUUID = applicationInstanceUUID;
-    }
-
-    public String getPublicKey() {
-        return publicKey;
-    }
-
-    public void setPublicKey(String publicKey) {
-        this.publicKey = publicKey;
-    }
-
-    public List<GroupPurchase> getGroupPurchases() {
-        return groupPurchases;
-    }
-
-    public void setGroupPurchases(List<GroupPurchase> groupPurchases) {
-        this.groupPurchases = groupPurchases;
     }
 
     @Override()
