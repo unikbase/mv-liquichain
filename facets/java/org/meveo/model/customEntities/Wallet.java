4efe68295c5f288eb0318e234a6bb7de5aa0c2c1
meveo.admin <no-mail@meveo.com>
2022-03-22 23:06:23 +0000

    Add the cet java source file : Wallet.javain the module : mv-liquichain

--- facets/java/org/meveo/model/customEntities/Wallet.java
+++ facets/java/org/meveo/model/customEntities/Wallet.java
@@ -19,6 +19,8 @@ public class Wallet implements CustomEntity {
     @JsonIgnore()
     private DBStorageType storages;
 
+    private String publicKey;
+
     @Override()
     public String getUuid() {
         return uuid;
@@ -34,6 +36,14 @@ public class Wallet implements CustomEntity {
 
     public void setStorages(DBStorageType storages) {
         this.storages = storages;
+    }
+
+    public String getPublicKey() {
+        return publicKey;
+    }
+
+    public void setPublicKey(String publicKey) {
+        this.publicKey = publicKey;
     }
 
     @Override()
