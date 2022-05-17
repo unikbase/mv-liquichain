61393813c717c53fb1f80cd720752192ad47ced0
tonysviews <tonysviews@gmail.com>
2022-05-17 07:26:12 +0000

    Add the cet java source file : VerifiedEmail.javain the module : mv-liquichain

--- facets/java/org/meveo/model/customEntities/VerifiedEmail.java
+++ facets/java/org/meveo/model/customEntities/VerifiedEmail.java
@@ -3,7 +3,6 @@ package org.meveo.model.customEntities;
 import org.meveo.model.CustomEntity;
 import java.util.List;
 import org.meveo.model.persistence.DBStorageType;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 public class VerifiedEmail implements CustomEntity {
@@ -20,14 +19,6 @@ public class VerifiedEmail implements CustomEntity {
     @JsonIgnore()
     private DBStorageType storages;
 
-    @JsonProperty(required = true)
-    private String walletId;
-
-    private Boolean verified;
-
-    @JsonProperty(required = true)
-    private String email;
-
     @Override()
     public String getUuid() {
         return uuid;
@@ -43,30 +34,6 @@ public class VerifiedEmail implements CustomEntity {
 
     public void setStorages(DBStorageType storages) {
         this.storages = storages;
-    }
-
-    public String getWalletId() {
-        return walletId;
-    }
-
-    public void setWalletId(String walletId) {
-        this.walletId = walletId;
-    }
-
-    public Boolean getVerified() {
-        return verified;
-    }
-
-    public void setVerified(Boolean verified) {
-        this.verified = verified;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 
     @Override()
