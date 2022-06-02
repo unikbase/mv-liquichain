c1dfd261bb3117031df9d125694c1eb7efd0a11a
meveo.admin <no-mail@meveo.com>
2022-06-02 09:05:41 +0000

    Add the cet java source file : Wallet.javain the module : mv-liquichain

--- facets/java/org/meveo/model/customEntities/Wallet.java
+++ facets/java/org/meveo/model/customEntities/Wallet.java
@@ -3,12 +3,6 @@ package org.meveo.model.customEntities;
 import org.meveo.model.CustomEntity;
 import java.util.List;
 import org.meveo.model.persistence.DBStorageType;
-import org.meveo.model.customEntities.VerifiedEmail;
-import org.meveo.model.customEntities.VerifiedPhoneNumber;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import org.meveo.model.customEntities.LiquichainApp;
-import org.meveo.model.customEntities.GroupPurchase;
-import java.util.ArrayList;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 public class Wallet implements CustomEntity {
@@ -25,40 +19,6 @@ public class Wallet implements CustomEntity {
     @JsonIgnore()
     private DBStorageType storages;
 
-    private String accountHash;
-
-    private String publicInfo;
-
-    private Boolean verified;
-
-    private String applicationInstanceUUID;
-
-    private String publicKey;
-
-    private String privateInfo;
-
-    private String privateKey;
-
-    private VerifiedEmail emailAddress;
-
-    private VerifiedPhoneNumber phoneNumber;
-
-    @JsonProperty(required = true)
-    private LiquichainApp application;
-
-    private String balance;
-
-    private String hexHash;
-
-    private Long lastPrivateInfoRequest;
-
-    @JsonProperty(required = true)
-    private String name;
-
-    private String keyPair;
-
-    private List<GroupPurchase> groupPurchases = new ArrayList<>();
-
     @Override()
     public String getUuid() {
         return uuid;
@@ -74,134 +34,6 @@ public class Wallet implements CustomEntity {
 
     public void setStorages(DBStorageType storages) {
         this.storages = storages;
-    }
-
-    public String getAccountHash() {
-        return accountHash;
-    }
-
-    public void setAccountHash(String accountHash) {
-        this.accountHash = accountHash;
-    }
-
-    public String getPublicInfo() {
-        return publicInfo;
-    }
-
-    public void setPublicInfo(String publicInfo) {
-        this.publicInfo = publicInfo;
-    }
-
-    public Boolean getVerified() {
-        return verified;
-    }
-
-    public void setVerified(Boolean verified) {
-        this.verified = verified;
-    }
-
-    public String getApplicationInstanceUUID() {
-        return applicationInstanceUUID;
-    }
-
-    public void setApplicationInstanceUUID(String applicationInstanceUUID) {
-        this.applicationInstanceUUID = applicationInstanceUUID;
-    }
-
-    public String getPublicKey() {
-        return publicKey;
-    }
-
-    public void setPublicKey(String publicKey) {
-        this.publicKey = publicKey;
-    }
-
-    public String getPrivateInfo() {
-        return privateInfo;
-    }
-
-    public void setPrivateInfo(String privateInfo) {
-        this.privateInfo = privateInfo;
-    }
-
-    public String getPrivateKey() {
-        return privateKey;
-    }
-
-    public void setPrivateKey(String privateKey) {
-        this.privateKey = privateKey;
-    }
-
-    public VerifiedEmail getEmailAddress() {
-        return emailAddress;
-    }
-
-    public void setEmailAddress(VerifiedEmail emailAddress) {
-        this.emailAddress = emailAddress;
-    }
-
-    public VerifiedPhoneNumber getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(VerifiedPhoneNumber phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public LiquichainApp getApplication() {
-        return application;
-    }
-
-    public void setApplication(LiquichainApp application) {
-        this.application = application;
-    }
-
-    public String getBalance() {
-        return balance;
-    }
-
-    public void setBalance(String balance) {
-        this.balance = balance;
-    }
-
-    public String getHexHash() {
-        return hexHash;
-    }
-
-    public void setHexHash(String hexHash) {
-        this.hexHash = hexHash;
-    }
-
-    public Long getLastPrivateInfoRequest() {
-        return lastPrivateInfoRequest;
-    }
-
-    public void setLastPrivateInfoRequest(Long lastPrivateInfoRequest) {
-        this.lastPrivateInfoRequest = lastPrivateInfoRequest;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getKeyPair() {
-        return keyPair;
-    }
-
-    public void setKeyPair(String keyPair) {
-        this.keyPair = keyPair;
-    }
-
-    public List<GroupPurchase> getGroupPurchases() {
-        return groupPurchases;
-    }
-
-    public void setGroupPurchases(List<GroupPurchase> groupPurchases) {
-        this.groupPurchases = groupPurchases;
     }
 
     @Override()
