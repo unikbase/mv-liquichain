d1978002abda22807622c2016393fc75ec585585
Tony Alejandro <tonysviews@gmail.com>
2022-06-17 17:21:12 +0800

    RE https://github.com/telecelplay/home/issues/330 - force update

--- facets/java/io/liquichain/api/core/AddAppUserRole.java
+++ facets/java/io/liquichain/api/core/AddAppUserRole.java
@@ -9,13 +9,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class AddAppUserRole extends Script {
-  private static final Logger LOG = LoggerFactory.getLogger(AddAppUserRole.class);
-	
-	@Override
-	public void execute(Map<String, Object> parameters) throws BusinessException {
-      	LOG.info("=================================================");
-		LOG.info("=============== ADD APP USER ROLE ===============");
-      	LOG.info("=================================================");
-	}
-	
+    private static final Logger LOG = LoggerFactory.getLogger(AddAppUserRole.class);
+
+    @Override
+    public void execute(Map<String, Object> parameters) throws BusinessException {
+        LOG.info("=================================================");
+        LOG.info("=============== ADD APP USER ROLE ===============");
+        LOG.info("=================================================");
+    }
+
 }
