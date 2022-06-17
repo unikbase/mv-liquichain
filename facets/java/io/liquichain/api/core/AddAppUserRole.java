e6595094256f2261d16fd53339a6a8bd5e0a85de
Tony Alejandro <tonysviews@gmail.com>
2022-06-17 18:06:09 +0800

    RE https://github.com/telecelplay/home/issues/330 - implement Role creation

--- facets/java/io/liquichain/api/core/AddAppUserRole.java
+++ facets/java/io/liquichain/api/core/AddAppUserRole.java
@@ -2,6 +2,8 @@ package io.liquichain.api.core;
 
 import java.util.Map;
 
+import org.meveo.model.security.Role;
+import org.meveo.service.admin.impl.RoleService;
 import org.meveo.service.script.Script;
 import org.meveo.admin.exception.BusinessException;
 
@@ -10,12 +12,19 @@ import org.slf4j.LoggerFactory;
 
 public class AddAppUserRole extends Script {
     private static final Logger LOG = LoggerFactory.getLogger(AddAppUserRole.class);
+    private final RoleService roleService = getCDIBean(RoleService.class);
 
     @Override
     public void execute(Map<String, Object> parameters) throws BusinessException {
         LOG.info("=================================================");
         LOG.info("=============== ADD APP USER ROLE ===============");
         LOG.info("=================================================");
+
+        Role role = new Role();
+        role.setName("APP_USER");
+        role.setDescription("App ");
+
+        roleService.create(role);
     }
 
 }
