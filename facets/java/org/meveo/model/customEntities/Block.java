4e8aceaca69e0e5c2462376ba75285f8dadeeb91
tonysviews <tonysviews@gmail.com>
2022-06-22 02:26:23 +0000

    Add the cet java source file : Block.javain the module : mv-liquichain null

--- facets/java/org/meveo/model/customEntities/Block.java
+++ facets/java/org/meveo/model/customEntities/Block.java
@@ -20,6 +20,8 @@ public class Block implements CustomEntity {
     @JsonIgnore()
     private DBStorageType storages;
 
+    private Long size;
+
     private Long blockNumber;
 
     private Instant creationDate;
@@ -43,6 +45,14 @@ public class Block implements CustomEntity {
 
     public void setStorages(DBStorageType storages) {
         this.storages = storages;
+    }
+
+    public Long getSize() {
+        return size;
+    }
+
+    public void setSize(Long size) {
+        this.size = size;
     }
 
     public Long getBlockNumber() {
