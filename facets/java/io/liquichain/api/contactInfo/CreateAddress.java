673da2d29bd11f14dc7e359dea3ec98a316a7da8
tonysviews <tonysviews@gmail.com>
2022-07-12 01:53:28 +0000

    Add the script File : io.liquichain.api.contactInfo.CreateAddressin the module : mv-liquichain

---
+++ facets/java/io/liquichain/api/contactInfo/CreateAddress.java
@@ -0,0 +1,15 @@
+package io.liquichain.api.contactInfo;
+
+import java.util.Map;
+
+import org.meveo.service.script.Script;
+import org.meveo.admin.exception.BusinessException;
+
+public class CreateAddress extends Script {
+	
+	@Override
+	public void execute(Map<String, Object> parameters) throws BusinessException {
+		super.execute(parameters);
+	}
+	
+}
