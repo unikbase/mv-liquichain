fe12e6994ff32d36c7d80a0194bbda7df4b98ad7
tonysviews <tonysviews@gmail.com>
2022-08-10 06:51:09 +0000

    Add the cet java source file : Address.javain the module : mv-liquichain

--- facets/java/org/meveo/model/customEntities/Address.java
+++ facets/java/org/meveo/model/customEntities/Address.java
@@ -3,8 +3,6 @@ package org.meveo.model.customEntities;
 import org.meveo.model.CustomEntity;
 import java.util.List;
 import org.meveo.model.persistence.DBStorageType;
-import org.meveo.model.customEntities.VerifiedPhoneNumber;
-import org.meveo.model.customEntities.Wallet;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 public class Address implements CustomEntity {
@@ -21,30 +19,6 @@ public class Address implements CustomEntity {
     @JsonIgnore()
     private DBStorageType storages;
 
-    private String country;
-
-    private VerifiedPhoneNumber phoneNumber;
-
-    private Wallet wallet;
-
-    private String city;
-
-    private String streetAddress;
-
-    private String countryCode;
-
-    private String dialCode;
-
-    private Double latitude;
-
-    private String postalCode;
-
-    private String name;
-
-    private String state;
-
-    private Double longitude;
-
     @Override()
     public String getUuid() {
         return uuid;
@@ -60,102 +34,6 @@ public class Address implements CustomEntity {
 
     public void setStorages(DBStorageType storages) {
         this.storages = storages;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public VerifiedPhoneNumber getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(VerifiedPhoneNumber phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public Wallet getWallet() {
-        return wallet;
-    }
-
-    public void setWallet(Wallet wallet) {
-        this.wallet = wallet;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getStreetAddress() {
-        return streetAddress;
-    }
-
-    public void setStreetAddress(String streetAddress) {
-        this.streetAddress = streetAddress;
-    }
-
-    public String getCountryCode() {
-        return countryCode;
-    }
-
-    public void setCountryCode(String countryCode) {
-        this.countryCode = countryCode;
-    }
-
-    public String getDialCode() {
-        return dialCode;
-    }
-
-    public void setDialCode(String dialCode) {
-        this.dialCode = dialCode;
-    }
-
-    public Double getLatitude() {
-        return latitude;
-    }
-
-    public void setLatitude(Double latitude) {
-        this.latitude = latitude;
-    }
-
-    public String getPostalCode() {
-        return postalCode;
-    }
-
-    public void setPostalCode(String postalCode) {
-        this.postalCode = postalCode;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public Double getLongitude() {
-        return longitude;
-    }
-
-    public void setLongitude(Double longitude) {
-        this.longitude = longitude;
     }
 
     @Override()
