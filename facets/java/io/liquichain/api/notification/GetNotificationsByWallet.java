060f4f96b6876631685f56ce510d81b71828222f
tonysviews <tonysviews@gmail.com>
2022-08-23 08:37:29 +0000

    Add the script File : io.liquichain.api.notification.GetNotificationsByWalletin the module : mv-liquichain

---
+++ facets/java/io/liquichain/api/notification/GetNotificationsByWallet.java
@@ -0,0 +1,15 @@
+package io.liquichain.api.notification;
+
+import java.util.Map;
+
+import org.meveo.service.script.Script;
+import org.meveo.admin.exception.BusinessException;
+
+public class GetNotificationsByWallet extends Script {
+	
+	@Override
+	public void execute(Map<String, Object> parameters) throws BusinessException {
+		super.execute(parameters);
+	}
+	
+}
