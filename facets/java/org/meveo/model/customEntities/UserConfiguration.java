65ffe48ceb70fac9b19d878180c45d20ed093449
babardev <babar.virk@gmail.com>
2022-08-23 14:05:31 +0000

    Add the cet java source file : UserConfiguration.javain the module : mv-liquichain https://github.com/telecelplay/mobileapp/issues/779

--- facets/java/org/meveo/model/customEntities/UserConfiguration.java
+++ facets/java/org/meveo/model/customEntities/UserConfiguration.java
@@ -22,6 +22,8 @@ public class UserConfiguration implements CustomEntity {
     @JsonIgnore()
     private DBStorageType storages;
 
+    private Boolean isChatNotificationsEnabled;
+
     private Boolean IsSellerInfoUpdatesEnabled;
 
     private Map<String, String> configurations = new HashMap<>();
@@ -47,6 +49,14 @@ public class UserConfiguration implements CustomEntity {
 
     public void setStorages(DBStorageType storages) {
         this.storages = storages;
+    }
+
+    public Boolean getIsChatNotificationsEnabled() {
+        return isChatNotificationsEnabled;
+    }
+
+    public void setIsChatNotificationsEnabled(Boolean isChatNotificationsEnabled) {
+        this.isChatNotificationsEnabled = isChatNotificationsEnabled;
     }
 
     public Boolean getIsSellerInfoUpdatesEnabled() {
