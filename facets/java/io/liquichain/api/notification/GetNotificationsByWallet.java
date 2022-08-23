3689a1ad94a90b062ca885764e346ec23df19100
Tony Alejandro <tonysviews@gmail.com>
2022-08-23 17:02:49 +0800

    RE: https://github.com/telecelplay/mobileapp/issues/729 - implement GetNotificationsByWallet script

--- facets/java/io/liquichain/api/notification/GetNotificationsByWallet.java
+++ facets/java/io/liquichain/api/notification/GetNotificationsByWallet.java
@@ -1,15 +1,87 @@
 package io.liquichain.api.notification;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import org.meveo.api.persistence.CrossStorageApi;
+import org.meveo.model.customEntities.Notification;
+import org.meveo.model.customEntities.VerifiedPhoneNumber;
+import org.meveo.model.customEntities.Wallet;
+import org.meveo.model.storage.Repository;
 import org.meveo.service.script.Script;
 import org.meveo.admin.exception.BusinessException;
+import org.meveo.service.storage.RepositoryService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class GetNotificationsByWallet extends Script {
-	
-	@Override
-	public void execute(Map<String, Object> parameters) throws BusinessException {
-		super.execute(parameters);
-	}
-	
+    private static final Logger LOG = LoggerFactory.getLogger(GetNotificationsByWallet.class);
+    private final CrossStorageApi crossStorageApi = getCDIBean(CrossStorageApi.class);
+    private final RepositoryService repositoryService = getCDIBean(RepositoryService.class);
+    private final Repository defaultRepo = repositoryService.findDefaultRepository();
+
+    private String walletId;
+    private final Map<String, Object> result = new HashMap<>() {{
+        put("status", "fail");
+    }};
+
+    public void setWalletId(String walletId) {
+        this.walletId = (walletId.startsWith("0x") ? walletId.substring(2) : walletId).toLowerCase();
+        ;
+    }
+
+    public Map<String, Object> getResult() {
+        return result;
+    }
+
+    @Override
+    public void execute(Map<String, Object> parameters) throws BusinessException {
+        super.execute(parameters);
+
+        Wallet wallet;
+        try {
+            wallet = crossStorageApi.find(defaultRepo, walletId, Wallet.class);
+        } catch (Exception e) {
+            String errorMessage = "Wallet does not exist: " + walletId;
+            LOG.error(errorMessage, e);
+            result.put("result", errorMessage);
+            return;
+        }
+
+        if (wallet == null) {
+            String errorMessage = "Wallet does not exist: " + walletId;
+            result.put("result", errorMessage);
+            return;
+        }
+
+        if (wallet.getPhoneNumber() == null || wallet.getPhoneNumber().getUuid() == null) {
+            String errorMessage = "Wallet does not have a registered phone number: " + walletId;
+            result.put("result", errorMessage);
+            return;
+        }
+
+        VerifiedPhoneNumber verifiedPhoneNumber;
+        try {
+            verifiedPhoneNumber =
+                crossStorageApi.find(defaultRepo, wallet.getPhoneNumber().getUuid(), VerifiedPhoneNumber.class);
+        } catch (Exception e) {
+            String errorMessage = "Failed to retrieve wallet phone number: " + walletId;
+            result.put("result", errorMessage);
+            return;
+        }
+
+        if (verifiedPhoneNumber == null || verifiedPhoneNumber.getPhoneNumber() == null) {
+            String errorMessage = "Phone number does not exist for wallet: " + walletId;
+            result.put("result", errorMessage);
+            return;
+        }
+
+        List<Notification> notifications = crossStorageApi.find(defaultRepo, Notification.class)
+                                                          .by("recipient", verifiedPhoneNumber.getPhoneNumber())
+                                                          .getResults();
+        result.put("status", "success");
+        result.put("result", notifications);
+    }
+
 }
