c90b65a80d75a26a8e65f08e53b1b6fd2482a685
babardev <babar.virk@gmail.com>
2022-08-29 14:28:58 +0000

    Add the script File : io.liquichain.api.config.UserUtilsin the module : mv-liquichain https://github.com/telecelplay/mobileapp/issues/779

--- facets/java/io/liquichain/api/config/UserUtils.java
+++ facets/java/io/liquichain/api/config/UserUtils.java
@@ -2,14 +2,35 @@ package io.liquichain.api.config;
 
 import java.util.Map;
 
+import org.meveo.api.persistence.CrossStorageApi;
+import org.meveo.model.storage.Repository;
+
 import org.meveo.service.script.Script;
 import org.meveo.admin.exception.BusinessException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class UserUtils extends Script {
 	
-	@Override
+    private static final Logger log = LoggerFactory.getLogger(UserUtils.class);
+
+    private CrossStorageApi crossStorageApi;
+    private Repository defaultRepo;
+
+    public UserUtils() {
+        super();
+    }
+
+    public UserUtils(CrossStorageApi crossStorageApi, Repository defaultRepo) {
+        this.crossStorageApi = crossStorageApi;
+        this.defaultRepo = defaultRepo;
+    }
+  
+  
+  	@Override
 	public void execute(Map<String, Object> parameters) throws BusinessException {
 		super.execute(parameters);
 	}
 	
+  	
 }
