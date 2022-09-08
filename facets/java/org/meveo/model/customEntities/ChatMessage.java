2669b2de110c9240ca08ddc9bda914c6509cca78
tonysviews <tonysviews@gmail.com>
2022-09-08 05:54:19 +0000

    Add the cet java source file : ChatMessage.javain the module : mv-liquichain

--- facets/java/org/meveo/model/customEntities/ChatMessage.java
+++ facets/java/org/meveo/model/customEntities/ChatMessage.java
@@ -3,9 +3,6 @@ package org.meveo.model.customEntities;
 import org.meveo.model.CustomEntity;
 import java.util.List;
 import org.meveo.model.persistence.DBStorageType;
-import org.meveo.model.customEntities.Wallet;
-import org.meveo.model.customEntities.ChatConversation;
-import java.time.Instant;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 public class ChatMessage implements CustomEntity {
@@ -22,14 +19,6 @@ public class ChatMessage implements CustomEntity {
     @JsonIgnore()
     private DBStorageType storages;
 
-    private Wallet sender;
-
-    private ChatConversation chatConversation;
-
-    private Instant creationDate;
-
-    private String message;
-
     @Override()
     public String getUuid() {
         return uuid;
@@ -45,38 +34,6 @@ public class ChatMessage implements CustomEntity {
 
     public void setStorages(DBStorageType storages) {
         this.storages = storages;
-    }
-
-    public Wallet getSender() {
-        return sender;
-    }
-
-    public void setSender(Wallet sender) {
-        this.sender = sender;
-    }
-
-    public ChatConversation getChatConversation() {
-        return chatConversation;
-    }
-
-    public void setChatConversation(ChatConversation chatConversation) {
-        this.chatConversation = chatConversation;
-    }
-
-    public Instant getCreationDate() {
-        return creationDate;
-    }
-
-    public void setCreationDate(Instant creationDate) {
-        this.creationDate = creationDate;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
     }
 
     @Override()
