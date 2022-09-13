ad2b45047dc17fff498f9f739a236f686f8144f9
tonysviews <tonysviews@gmail.com>
2022-09-13 23:09:49 +0000

    Add the cet java source file : LiquichainApp.javain the module : mv-liquichain

--- facets/java/org/meveo/model/customEntities/LiquichainApp.java
+++ facets/java/org/meveo/model/customEntities/LiquichainApp.java
@@ -3,6 +3,7 @@ package org.meveo.model.customEntities;
 import org.meveo.model.CustomEntity;
 import java.util.List;
 import org.meveo.model.persistence.DBStorageType;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 public class LiquichainApp implements CustomEntity {
@@ -20,6 +21,9 @@ public class LiquichainApp implements CustomEntity {
     private DBStorageType storages;
 
     private String hexCode;
+
+    @JsonProperty(required = true)
+    private String name;
 
     @Override()
     public String getUuid() {
@@ -44,6 +48,14 @@ public class LiquichainApp implements CustomEntity {
 
     public void setHexCode(String hexCode) {
         this.hexCode = hexCode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     @Override()
