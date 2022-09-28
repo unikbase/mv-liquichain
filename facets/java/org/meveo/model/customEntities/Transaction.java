ac54e41d6882aa307636a0d7116a30925c17896e
tonysviews <tonysviews@gmail.com>
2022-09-28 00:16:17 +0000

    Add the cet java source file : Transaction.javain the module : mv-liquichain

--- facets/java/org/meveo/model/customEntities/Transaction.java
+++ facets/java/org/meveo/model/customEntities/Transaction.java
@@ -31,6 +31,8 @@ public class Transaction implements CustomEntity {
 
     private String fromHexHash;
 
+    private String description;
+
     private Long transactionIndex;
 
     private Instant creationDate;
@@ -116,6 +118,14 @@ public class Transaction implements CustomEntity {
 
     public void setFromHexHash(String fromHexHash) {
         this.fromHexHash = fromHexHash;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     public Long getTransactionIndex() {
