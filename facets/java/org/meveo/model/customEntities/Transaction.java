0b9cabf00407c52e40a3dd984edc35ad47560562
tonysviews <tonysviews@gmail.com>
2022-09-28 00:16:16 +0000

    Add the cet java source file : Transaction.javain the module : mv-liquichain

--- facets/java/org/meveo/model/customEntities/Transaction.java
+++ facets/java/org/meveo/model/customEntities/Transaction.java
@@ -44,6 +44,8 @@ public class Transaction implements CustomEntity {
 
     private Instant expirationDate;
 
+    private String gasPrice;
+
     @Override()
     public String getUuid() {
         return uuid;
@@ -147,6 +149,14 @@ public class Transaction implements CustomEntity {
 
     public void setExpirationDate(Instant expirationDate) {
         this.expirationDate = expirationDate;
+    }
+
+    public String getGasPrice() {
+        return gasPrice;
+    }
+
+    public void setGasPrice(String gasPrice) {
+        this.gasPrice = gasPrice;
     }
 
     @Override()
