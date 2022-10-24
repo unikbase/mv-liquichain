8a7961e3b2db97801f4b544eb057b5dd7f01d97f
tonysviews <tonysviews@gmail.com>
2022-10-24 04:07:20 +0000

    Add the cet java source file : Address.javain the module : mv-liquichain

--- facets/java/org/meveo/model/customEntities/Address.java
+++ facets/java/org/meveo/model/customEntities/Address.java
@@ -23,8 +23,6 @@ public class Address implements CustomEntity {
 
     private String country;
 
-    private String notes;
-
     private Wallet wallet;
 
     private String city;
@@ -35,7 +33,9 @@ public class Address implements CustomEntity {
 
     private String postalCode;
 
-    private Boolean isDefault;
+    private String externalId;
+
+    private String type;
 
     private VerifiedPhoneNumber phoneNumber;
 
@@ -72,14 +72,6 @@ public class Address implements CustomEntity {
 
     public void setCountry(String country) {
         this.country = country;
-    }
-
-    public String getNotes() {
-        return notes;
-    }
-
-    public void setNotes(String notes) {
-        this.notes = notes;
     }
 
     public Wallet getWallet() {
@@ -122,12 +114,20 @@ public class Address implements CustomEntity {
         this.postalCode = postalCode;
     }
 
-    public Boolean getIsDefault() {
-        return isDefault;
+    public String getExternalId() {
+        return externalId;
     }
 
-    public void setIsDefault(Boolean isDefault) {
-        this.isDefault = isDefault;
+    public void setExternalId(String externalId) {
+        this.externalId = externalId;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
     }
 
     public VerifiedPhoneNumber getPhoneNumber() {
