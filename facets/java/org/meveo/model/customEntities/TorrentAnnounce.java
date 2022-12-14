28ff817fcf59e52600acce3420ff54d2a34e1081
tonysviews <tonysviews@gmail.com>
2022-12-14 05:46:34 +0000

    Add the cet java source file : TorrentAnnounce.javain the module : mv-liquichain

--- facets/java/org/meveo/model/customEntities/TorrentAnnounce.java
+++ facets/java/org/meveo/model/customEntities/TorrentAnnounce.java
@@ -3,6 +3,7 @@ package org.meveo.model.customEntities;
 import org.meveo.model.CustomEntity;
 import java.util.List;
 import org.meveo.model.persistence.DBStorageType;
+import org.meveo.model.customEntities.Wallet;
 import java.time.Instant;
 import org.meveo.model.customEntities.LiquichainApp;
 import com.fasterxml.jackson.annotation.JsonIgnore;
@@ -22,6 +23,8 @@ public class TorrentAnnounce implements CustomEntity {
     private DBStorageType storages;
 
     private String peerId;
+
+    private Wallet wallet;
 
     private String ip;
 
@@ -72,6 +75,14 @@ public class TorrentAnnounce implements CustomEntity {
 
     public void setPeerId(String peerId) {
         this.peerId = peerId;
+    }
+
+    public Wallet getWallet() {
+        return wallet;
+    }
+
+    public void setWallet(Wallet wallet) {
+        this.wallet = wallet;
     }
 
     public String getIp() {
