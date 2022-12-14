0785c261bca581c834afc4f06e878779523dbccd
tonysviews <tonysviews@gmail.com>
2022-12-14 05:46:25 +0000

    Add the cet java source file : UserConfiguration.javain the module : mv-liquichain

--- facets/java/org/meveo/model/customEntities/UserConfiguration.java
+++ facets/java/org/meveo/model/customEntities/UserConfiguration.java
@@ -3,7 +3,6 @@ package org.meveo.model.customEntities;
 import org.meveo.model.CustomEntity;
 import java.util.List;
 import org.meveo.model.persistence.DBStorageType;
-import org.meveo.model.customEntities.Wallet;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 public class UserConfiguration implements CustomEntity {
@@ -20,22 +19,6 @@ public class UserConfiguration implements CustomEntity {
     @JsonIgnore()
     private DBStorageType storages;
 
-    private String autoReplyMessage;
-
-    private Boolean isChatNotificationsEnabled;
-
-    private Boolean isChatEnabledFromProfilePage;
-
-    private Boolean isEmailNotificationsEnabled;
-
-    private Boolean isOrderUpdatesEnabled;
-
-    private Boolean isSellerInfoUpdatesEnabled;
-
-    private Boolean isAutoReplyEnabled;
-
-    private Wallet user;
-
     @Override()
     public String getUuid() {
         return uuid;
@@ -51,70 +34,6 @@ public class UserConfiguration implements CustomEntity {
 
     public void setStorages(DBStorageType storages) {
         this.storages = storages;
-    }
-
-    public String getAutoReplyMessage() {
-        return autoReplyMessage;
-    }
-
-    public void setAutoReplyMessage(String autoReplyMessage) {
-        this.autoReplyMessage = autoReplyMessage;
-    }
-
-    public Boolean getIsChatNotificationsEnabled() {
-        return isChatNotificationsEnabled;
-    }
-
-    public void setIsChatNotificationsEnabled(Boolean isChatNotificationsEnabled) {
-        this.isChatNotificationsEnabled = isChatNotificationsEnabled;
-    }
-
-    public Boolean getIsChatEnabledFromProfilePage() {
-        return isChatEnabledFromProfilePage;
-    }
-
-    public void setIsChatEnabledFromProfilePage(Boolean isChatEnabledFromProfilePage) {
-        this.isChatEnabledFromProfilePage = isChatEnabledFromProfilePage;
-    }
-
-    public Boolean getIsEmailNotificationsEnabled() {
-        return isEmailNotificationsEnabled;
-    }
-
-    public void setIsEmailNotificationsEnabled(Boolean isEmailNotificationsEnabled) {
-        this.isEmailNotificationsEnabled = isEmailNotificationsEnabled;
-    }
-
-    public Boolean getIsOrderUpdatesEnabled() {
-        return isOrderUpdatesEnabled;
-    }
-
-    public void setIsOrderUpdatesEnabled(Boolean isOrderUpdatesEnabled) {
-        this.isOrderUpdatesEnabled = isOrderUpdatesEnabled;
-    }
-
-    public Boolean getIsSellerInfoUpdatesEnabled() {
-        return isSellerInfoUpdatesEnabled;
-    }
-
-    public void setIsSellerInfoUpdatesEnabled(Boolean isSellerInfoUpdatesEnabled) {
-        this.isSellerInfoUpdatesEnabled = isSellerInfoUpdatesEnabled;
-    }
-
-    public Boolean getIsAutoReplyEnabled() {
-        return isAutoReplyEnabled;
-    }
-
-    public void setIsAutoReplyEnabled(Boolean isAutoReplyEnabled) {
-        this.isAutoReplyEnabled = isAutoReplyEnabled;
-    }
-
-    public Wallet getUser() {
-        return user;
-    }
-
-    public void setUser(Wallet user) {
-        this.user = user;
     }
 
     @Override()
