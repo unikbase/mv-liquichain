6ea0f7e2018b91c71d43448d7d4a6f002a88092f
tonysviews <tonysviews@gmail.com>
2022-12-27 08:11:24 +0800

    Create JS script for endpointgetUserContactInfo

--- facets/javascript/endpoints/getUserContactInfo.js
+++ facets/javascript/endpoints/getUserContactInfo.js
@@ -1,28 +1,53 @@
-const getUserContactInfo = async (parameters) =>  {
-	const baseUrl = window.location.origin;
-	const url = new URL(`${window.location.pathname.split('/')[1]}/rest/getUserContactInfo/`, baseUrl);
-	return fetch(url.toString(), {
-		method: 'GET'
-	});
+import EndpointInterface from "#{API_BASE_URL}/api/rest/endpoint/EndpointInterface.js";
+
+// the request schema, this should be updated
+// whenever changes to the endpoint parameters are made
+// this is important because this is used to validate and parse the request parameters
+const requestSchema = {
+  "title" : "getUserContactInfoRequest",
+  "id" : "getUserContactInfoRequest",
+  "default" : "Schema definition for getUserContactInfo",
+  "$schema" : "http://json-schema.org/draft-07/schema",
+  "type" : "object"
 }
 
-const getUserContactInfoForm = (container) => {
-	const html = `<form id='getUserContactInfo-form'>
-		<button type='button'>Test</button>
-	</form>`;
-
-	container.insertAdjacentHTML('beforeend', html)
-
-
-	container.querySelector('#getUserContactInfo-form button').onclick = () => {
-		const params = {
-
-		};
-
-		getUserContactInfo(params).then(r => r.text().then(
-				t => alert(t)
-			));
-	};
+// the response schema, this should be updated
+// whenever changes to the endpoint parameters are made
+// this is important because this could be used to parse the result
+const responseSchema = {
+  "title" : "getUserContactInfoResponse",
+  "id" : "getUserContactInfoResponse",
+  "default" : "Schema definition for getUserContactInfo",
+  "$schema" : "http://json-schema.org/draft-07/schema",
+  "type" : "object",
+  "properties" : {
+    "result" : {
+      "title" : "result",
+      "type" : "string",
+      "minLength" : 1
+    }
+  }
 }
 
-export { getUserContactInfo, getUserContactInfoForm };
+// should contain offline mock data, make sure it adheres to the response schema
+const mockResult = {};
+
+class getUserContactInfo extends EndpointInterface {
+	constructor() {
+		// name and http method, these are inserted when code is generated
+		super("getUserContactInfo", "GET");
+		this.requestSchema = requestSchema;
+		this.responseSchema = responseSchema;
+		this.mockResult = mockResult;
+	}
+
+	getRequestSchema() {
+		return this.requestSchema;
+	}
+
+	getResponseSchema() {
+		return this.responseSchema;
+	}
+}
+
+export default new getUserContactInfo();
