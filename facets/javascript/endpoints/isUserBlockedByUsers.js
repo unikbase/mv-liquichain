4360af77d6bac47324e0c6b109d923a0d057094e
babardev <babar.virk@gmail.com>
2023-01-10 12:33:26 +0000

    Create JS script for endpointisUserBlockedByUsers https://github.com/telecelplay/mv-liquichain/issues/14 confirmation message on register action

--- facets/javascript/endpoints/isUserBlockedByUsers.js
+++ facets/javascript/endpoints/isUserBlockedByUsers.js
@@ -1,48 +1,53 @@
-const isUserBlockedByUsers = async (parameters) =>  {
-	const baseUrl = window.location.origin;
-	const url = new URL(`${window.location.pathname.split('/')[1]}/rest/isUserBlockedByUsers/`, baseUrl);
-	if (parameters.walletId !== undefined) {
-		url.searchParams.append('walletId', parameters.walletId);
-	}
+import EndpointInterface from "#{API_BASE_URL}/api/rest/endpoint/EndpointInterface.js";
 
-	if (parameters.blockerWalletIds !== undefined) {
-	for(let v of parameters.blockerWalletIds){		url.searchParams.append('blockerWalletIds', v);
-	}
-	}
-
-	return fetch(url.toString(), {
-		method: 'GET'
-	});
+// the request schema, this should be updated
+// whenever changes to the endpoint parameters are made
+// this is important because this is used to validate and parse the request parameters
+const requestSchema = {
+  "title" : "isUserBlockedByUsersRequest",
+  "id" : "isUserBlockedByUsersRequest",
+  "default" : "Schema definition for isUserBlockedByUsers",
+  "$schema" : "http://json-schema.org/draft-07/schema",
+  "type" : "object"
 }
 
-const isUserBlockedByUsersForm = (container) => {
-	const html = `<form id='isUserBlockedByUsers-form'>
-		<div id='isUserBlockedByUsers-null-form-field'>
-			<label for='null'>null</label>
-			<input type='text' id='isUserBlockedByUsers-null-param' name='null'/>
-		</div>
-		<div id='isUserBlockedByUsers-null-form-field'>
-			<label for='null'>null</label>
-			<input type='text' id='isUserBlockedByUsers-null-param' name='null'/>
-		</div>
-		<button type='button'>Test</button>
-	</form>`;
-
-	container.insertAdjacentHTML('beforeend', html)
-
-	const null = container.querySelector('#isUserBlockedByUsers-null-param');
-	const null = container.querySelector('#isUserBlockedByUsers-null-param');
-
-	container.querySelector('#isUserBlockedByUsers-form button').onclick = () => {
-		const params = {
-			null : null.value !== "" ? null.value : undefined,
-			null : null.value !== "" ? null.value : undefined
-		};
-
-		isUserBlockedByUsers(params).then(r => r.text().then(
-				t => alert(t)
-			));
-	};
+// the response schema, this should be updated
+// whenever changes to the endpoint parameters are made
+// this is important because this could be used to parse the result
+const responseSchema = {
+  "title" : "isUserBlockedByUsersResponse",
+  "id" : "isUserBlockedByUsersResponse",
+  "default" : "Schema definition for isUserBlockedByUsers",
+  "$schema" : "http://json-schema.org/draft-07/schema",
+  "type" : "object",
+  "properties" : {
+    "result" : {
+      "title" : "result",
+      "type" : "string",
+      "minLength" : 1
+    }
+  }
 }
 
-export { isUserBlockedByUsers, isUserBlockedByUsersForm };
+// should contain offline mock data, make sure it adheres to the response schema
+const mockResult = {};
+
+class isUserBlockedByUsers extends EndpointInterface {
+	constructor() {
+		// name and http method, these are inserted when code is generated
+		super("isUserBlockedByUsers", "GET");
+		this.requestSchema = requestSchema;
+		this.responseSchema = responseSchema;
+		this.mockResult = mockResult;
+	}
+
+	getRequestSchema() {
+		return this.requestSchema;
+	}
+
+	getResponseSchema() {
+		return this.responseSchema;
+	}
+}
+
+export default new isUserBlockedByUsers();
